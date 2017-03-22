54d49136fb18e677d78961493b1244dbc2fe07fe
kvpratama <kvpratama@gmail.com>
2017-03-22 17:02:14 +0900

    Gradient descent for multivariate

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -6,7 +6,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
+tempTheta = theta;
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,16 +16,15 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    
+    for GDiter = 1:size(X, 2)
 
+        %tempTheta(GDiter) = theta(GDiter) - alpha * 1/m * sum((X * theta - y) .* X(:,GDiter));
+        tempTheta(GDiter) = theta(GDiter) - alpha * 1/m * (X * theta - y)' * X(:,GDiter);
 
-
-
-
-
-
-
-
-
+    end
+        
+    theta = tempTheta;
 
     % ============================================================
 
